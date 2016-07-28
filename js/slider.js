c48124cb924b1e9211be3029760ab55fcf175a5f
hundredvisionsguy <cwinikka@gmail.com>
2016-07-28 00:16:56 -0700

    cleaned up slider.js for tutorial

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,15 @@
-/* slider.js */
+/* slider.js 
+
+From: http://www.my-html-codes.com/easy-jquery-carousel/
+
+when page is loaded
+wait 5 seconds
+move the list of images left by the individual images width
+remove image from list
+add it to the end of the list
+then start the cycle again
+
+*/
 $(function(){
 	
 });
