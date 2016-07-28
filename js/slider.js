3474d2b323cc467af5766376b2b677f36d6bc450
hundredvisionsguy <cwinikka@gmail.com>
2016-07-28 16:31:06 -0700

    cleaned up pseudo-code

--- js/slider.js
+++ js/slider.js
@@ -4,12 +4,13 @@ From: http://www.my-html-codes.com/easy-jquery-carousel/
 
 when page is loaded
 wait 5 seconds
-move the list of images left by the individual images width
+move the list of images left by the individual images' width
 remove image from list
 add it to the end of the list
 then start the cycle again
 
 */
-$(function(){
-	
+
+$(document).ready( function() {
+	// Do something here.
 });
